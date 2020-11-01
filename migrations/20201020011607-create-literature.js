5419fd9f96860e3cc439280357039233ebd8f3f4
Nur Askiah <askiahnur1@gmail.com>
2020-11-01 23:13:52 +0800

    update relations

--- migrations/20201020011607-create-literature.js
+++ migrations/20201020011607-create-literature.js
@@ -1,58 +1,58 @@
-"use strict";
+'use strict';
 module.exports = {
-    up: async(queryInterface, Sequelize) => {
-        await queryInterface.createTable("Literature", {
-            id: {
-                allowNull: false,
-                autoIncrement: true,
-                primaryKey: true,
-                type: Sequelize.INTEGER
-            },
-            title: {
-                type: Sequelize.STRING
-            },
-            userId: {
-                allowNull: false,
-                type: Sequelize.INTEGER,
-                references: {
-                    model: "Users",
-                    as: "id"
-                },
-                onUpdate: "SET NULL",
-                onDelete: "CASCADE"
-            },
-            publication_date: {
-                type: Sequelize.DATE
-            },
-            pages: {
-                type: Sequelize.STRING
-            },
-            ISBN: {
-                type: Sequelize.STRING
-            },
-            author: {
-                type: Sequelize.STRING
-            },
-            status: {
-                type: Sequelize.STRING
-            },
-            file: {
-                type: Sequelize.STRING
-            },
-            thumbnail: {
-                type: Sequelize.STRING
-            },
-            createdAt: {
-                allowNull: false,
-                type: Sequelize.DATE
-            },
-            updatedAt: {
-                allowNull: false,
-                type: Sequelize.DATE
-            }
-        });
-    },
-    down: async(queryInterface, Sequelize) => {
-        await queryInterface.dropTable("Literature");
-    }
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Literature', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          as: 'user',
+        },
+        onUpdate: 'SET NULL',
+        onDelete: 'CASCADE',
+      },
+      publication_date: {
+        type: Sequelize.DATE,
+      },
+      pages: {
+        type: Sequelize.STRING,
+      },
+      ISBN: {
+        type: Sequelize.STRING,
+      },
+      author: {
+        type: Sequelize.STRING,
+      },
+      status: {
+        type: Sequelize.STRING,
+      },
+      file: {
+        type: Sequelize.STRING,
+      },
+      thumbnail: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Literature');
+  },
 };
--- models/library.js
+++ models/library.js
@@ -1,33 +1,36 @@
-"use strict";
-const { Model } = require("sequelize");
+'use strict';
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-    class Library extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
-        static associate(models) {
-            Library.belongsTo(models.Literature, {
-                as: "literature",
-                foreignKey: {
-                    name: "literatureId"
-                }
-            });
-            Library.belongsTo(models.User, {
-                as: "user",
-                foreignKey: {
-                    name: "userId"
-                }
-            });
-        }
+  class Library extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Library.belongsTo(models.Literature, {
+        as: 'literature',
+        foreignKey: {
+          name: 'literatureId',
+        },
+      });
+      Library.belongsTo(models.User, {
+        as: 'user',
+        foreignKey: {
+          name: 'userId',
+        },
+      });
     }
-    Library.init({
-        literatureId: DataTypes.INTEGER,
-        userId: DataTypes.INTEGER
-    }, {
-        sequelize,
-        modelName: "Library"
-    });
-    return Library;
+  }
+  Library.init(
+    {
+      literatureId: DataTypes.INTEGER,
+      userId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: 'Library',
+    }
+  );
+  return Library;
 };
