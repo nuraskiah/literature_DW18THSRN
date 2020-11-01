a6a535095bda2c852a32f0254072e80caece4731
Nur Askiah <askiahnur1@gmail.com>
2020-11-02 00:02:36 +0800

    update

---
+++ seeders/20201101155925-User.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          fullName: 'User',
+          email: 'user@gmail.com',
+          password: '123123123',
+          gender: 'Male',
+          phone: '081232123123',
+          address: 'Indonesia',
+          avatar: 'new.png',
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', null, {});
+  },
+};
